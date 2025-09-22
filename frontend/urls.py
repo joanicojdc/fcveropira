0fa31b7049b0f291ca11f38ea4da72685e419ac6
joanicojdc <joanicojdc@gmail.com>
2025-09-22 15:49:25 +0900

    update all menu link

--- frontend/urls.py
+++ frontend/urls.py
@@ -3,5 +3,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('profile', views.profile, name='profile'),
+    path('history', views.history, name='history'),
+    path('management', views.management, name='management'),
+    path('academy', views.academy, name='academy'),
+    path('first_team', views.first_team, name='first_team'),
+    path('reserves', views.reserves, name='reserves'),
+    path('youth_team', views.youth_team, name='youth_team'),
+    path('women_team', views.women_team, name='women_team'),
+    path('next_match', views.next_match, name='next_match'),
+    path('results', views.results, name='results'),
+    path('standings', views.standings, name='standings'), 
+    path('news', views.news, name='news'),
+    path('contact', views.contact, name='contact'),
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -7,5 +7,32 @@ from django.http import HttpResponse
 #     return HttpResponse("Hello! This is the frontend app running.")
 
 
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    return render(request, 'home.html')
+def profile(request):
+    return render(request, 'menu/profile.html')
+def history(request):
+    return render(request, 'menu/history.html')
+def management(request):
+    return render(request, 'menu/management.html')
+def academy(request):
+    return render(request, 'menu/academy.html')
+def first_team(request):
+    return render(request, 'menu/first_team.html')
+def reserves(request):
+    return render(request, 'menu/reserves.html')
+def youth_team(request):
+    return render(request, 'menu/youth_team.html')
+def women_team(request):
+    return render(request, 'menu/women_team.html')
+def next_match(request):
+    return render(request, 'menu/next_match.html')
+def results(request):
+    return render(request, 'menu/results.html')
+def standings(request):
+    return render(request, 'menu/standings.html')
+
+def news(request):
+    return render(request, 'menu/news.html')
+def contact(request):
+    return render(request, 'menu/contact.html')
